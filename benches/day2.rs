1e6b452c94ac945243fd18a34ffdfb175896fc6e
A1cey <mschelkle05@web.de>
2025-10-27 15:50:13 +0100

    Optimization day 2

--- benches/day2.rs
+++ benches/day2.rs
@@ -5,8 +5,10 @@ use advent_of_code_2023::day2;
 fn solutions(c: &mut Criterion) {
     let mut group = c.benchmark_group("Day 2 Solutions");
 
-    group.bench_function("day2 TODO", |b| {
-        b.iter(|| day2::solution1::solve())
+    group.bench_function("day2 iterator", |b| b.iter(|| day2::iterator::solve()));
+
+    group.bench_function("day2 iterator_with_early_return", |b| {
+        b.iter(|| day2::iterator_with_early_return::solve())
     });
 
     group.finish();
--- src/day2.rs
+++ src/day2.rs
@@ -1,18 +1,17 @@
 pub fn solve() {
-    let (part_one, part_two) = solution1::solve();
+    let (part_one, part_two) = iterator_with_early_return::solve();
 
+    assert_eq!(part_one, 2545, "Part 1 failed.");
+    assert_eq!(part_two, 78111, "Part 2 failed.");
     dbg!(part_one);
     dbg!(part_two);
 }
 
-pub mod solution1 {
+pub mod iterator {
     pub fn solve() -> (u32, u32) {
         let data = include_str!("../data/day2.txt");
 
-        let part_one: u32 = data
-            .lines()
-            .filter_map(part_one)
-            .sum();
+        let part_one: u32 = data.lines().filter_map(part_one).sum();
 
         let part_two: u32 = data.lines().map(part_two).sum();
 
@@ -21,8 +20,6 @@ pub mod solution1 {
 
     fn part_one(line: &str) -> Option<u32> {
         let (game, values) = line.split_once(": ").unwrap();
-
-        let game_id: u32 = game.strip_prefix("Game ").unwrap().parse().unwrap();
 
         values
             .split("; ")
@@ -41,7 +38,67 @@ pub mod solution1 {
                 })
             })
             .all(|valid| valid)
-            .then_some(game_id)
+            .then(|| game.strip_prefix("Game ").unwrap().parse().unwrap())
+    }
+
+    fn part_two(line: &str) -> u32 {
+        let mut max_red = 0;
+        let mut max_green = 0;
+        let mut max_blue = 0;
+
+        line.split_once(": ")
+            .unwrap()
+            .1
+            .split("; ")
+            .for_each(|draw| {
+                draw.split(", ").for_each(|color| {
+                    let (num_str, col) = color.split_once(" ").unwrap();
+
+                    let num = num_str.parse().unwrap();
+
+                    match col {
+                        "red" => max_red = max_red.max(num),
+                        "green" => max_green = max_green.max(num),
+                        "blue" => max_blue = max_blue.max(num),
+                        _ => unreachable!(),
+                    }
+                })
+            });
+
+        max_red * max_green * max_blue
+    }
+}
+
+pub mod iterator_with_early_return {
+    pub fn solve() -> (u32, u32) {
+        let data = include_str!("../data/day2.txt");
+
+        let part_one: u32 = data.lines().filter_map(part_one).sum();
+
+        let part_two: u32 = data.lines().map(part_two).sum();
+
+        (part_one, part_two)
+    }
+
+    fn part_one(line: &str) -> Option<u32> {
+        let (game, values) = line.split_once(": ").unwrap();
+
+        for color in values.split("; ").flat_map(|draw| draw.split(", ")) {
+            let (num_str, col) = color.split_once(" ").unwrap();
+
+            let num: u8 = num_str.parse().unwrap();
+
+            if match col {
+                "red" => num > 12,
+                "green" => num > 13,
+                "blue" => num > 14,
+                _ => unreachable!(),
+            } {
+                return None;
+            }
+        }
+
+        game.strip_prefix("Game ").unwrap().parse().ok()
     }
 
     fn part_two(line: &str) -> u32 {
