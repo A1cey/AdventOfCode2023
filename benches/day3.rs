39028d54223eab28267604b315227087d3abbb32
A1cey <mschelkle05@web.de>
2025-10-29 18:44:33 +0100

    Added benchmarks for all days

---
+++ benches/day3.rs
@@ -0,0 +1,14 @@
+use criterion::{Criterion, criterion_group, criterion_main};
+
+use advent_of_code_2023::day3;
+
+fn solutions(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Day 3 Solutions");
+
+    group.bench_function("day3 TODO", |b| b.iter(|| day3::solution1::solve()));
+
+    group.finish();
+}
+
+criterion_group!(benches, solutions);
+criterion_main!(benches);
---
+++ src/day3.rs
@@ -0,0 +1,17 @@
+pub fn solve() {
+    let (part_one, part_two) = solution1::solve();
+
+    dbg!(part_one);
+    dbg!(part_two);
+
+    assert_eq!(part_one, todo!());
+    assert_eq!(part_two, todo!());
+}
+
+pub mod solution1 {
+    pub fn solve() -> (u32, u32) {
+        let data = include_str!("../data/day3.txt");
+        
+        (0,0)
+    }
+}
